a72ce92f0d555630ce1ddd2bae99d53e88543f21
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2025-09-13 12:54:01 +0100

    Functional programming

---
+++ Linq/02-Functional Programming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_Functional_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Functinal programming , Monday InshaAllah");
+        }
+    }
+}
